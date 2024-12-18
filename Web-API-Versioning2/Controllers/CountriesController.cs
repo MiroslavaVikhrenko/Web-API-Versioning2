e60e1239540ef7ada4a043d73ce192aa7433482d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-18 16:11:06 -0700

    Add second version of the action method

--- Web-API-Versioning2/Controllers/CountriesController.cs
+++ Web-API-Versioning2/Controllers/CountriesController.cs
@@ -9,7 +9,7 @@ namespace Web_API_Versioning2.Controllers
     public class CountriesController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetV1()
         {
             var countriesDomainModel = CountriesData.Get();
 
@@ -21,6 +21,24 @@ namespace Web_API_Versioning2.Controllers
                 {
                     Id = country.Id,
                     Name = country.Name,
+                });
+            }
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IActionResult GetV2()
+        {
+            var countriesDomainModel = CountriesData.Get();
+
+            //Map Domain Model to DTO
+            var response = new List<CountryDtoV2>();
+            foreach (var country in countriesDomainModel)
+            {
+                response.Add(new CountryDtoV2
+                {
+                    Id = country.Id,
+                    CountryName = country.Name,
                 });
             }
             return Ok(response);
