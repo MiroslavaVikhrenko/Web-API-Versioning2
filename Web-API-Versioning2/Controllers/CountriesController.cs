7cddb1daf31720a2466aab2767ae0cc139218f4e
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-18 15:53:23 -0700

    Configure starting point for Countries API

---
+++ Web-API-Versioning2/Controllers/CountriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Versioning2.Models.DTOs;
+
+namespace Web_API_Versioning2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var countriesDomainModel = CountriesData.Get();
+
+            //Map Domain Model to DTO
+            var response = new List<CountryDto>();
+            foreach (var country in countriesDomainModel)
+            {
+                response.Add(new CountryDto
+                {
+                    Id = country.Id,
+                    Name = country.Name,
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
