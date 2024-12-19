e201094af94202f91df902fbf8d04706bd0cc913
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-18 17:19:29 -0700

    Add structure for implemented methods in ConfigureSwaggerOptions class

--- Web-API-Versioning2/ConfigureSwaggerOptions.cs
+++ Web-API-Versioning2/ConfigureSwaggerOptions.cs
@@ -1,6 +1,22 @@
-﻿namespace Web_API_Versioning2
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Web_API_Versioning2
 {
-    public class ConfigureSwaggerOptions
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
     {
+        
+        public void Configure(string? name, SwaggerGenOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var option in options)
+            {
+
+            }
+        }
     }
 }
