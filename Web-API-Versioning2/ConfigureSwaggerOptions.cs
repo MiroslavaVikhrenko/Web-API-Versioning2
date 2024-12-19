ea6c419c83dbafa2a9e93aca4acc7c037c51b457
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-18 17:21:30 -0700

    Inject the provider in ConfigureSwaggerOptions class

--- Web-API-Versioning2/ConfigureSwaggerOptions.cs
+++ Web-API-Versioning2/ConfigureSwaggerOptions.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Web_API_Versioning2
 {
     public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
     {
-        
+        //Inject the provider
+        private readonly IApiVersionDescriptionProvider apiVersionDescriptionProvider;
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            this.apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+        }
         public void Configure(string? name, SwaggerGenOptions options)
         {
             throw new NotImplementedException();
